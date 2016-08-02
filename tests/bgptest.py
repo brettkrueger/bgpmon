0063ce41a8fd7af0dbc3118cd2e24a8214347182
brett <admin@krux.us>
2016-08-02 16:00:02 -0400

    Tests update - 8/2/16 15:00

--- tests/bgptest.py
+++ tests/bgptest.py
@@ -7,7 +7,23 @@ except: print("Failed to import")
 email='demo@bgpmon.net'
 passwd='demo'
 
-ip="104.16.0.0/12"
+ip="103.201.129.0/24"
 bgpmon=BGPMon()
-test=bgpmon.getIpInfo(email,passwd,ip)
-print test[0]["prefix"]
+print("\ngetIpInfo():\n"+str(bgpmon.getIpInfo(email,passwd,ip)))
+print("\ngetASName():\n"+str(bgpmon.getASName(email,passwd,"AS134829")))
+print("\ngetPrefixesForAS():\n"+str(bgpmon.getPrefixesForAS(email,passwd,int(134829))))
+print("\ngetCountry():\n"+str(bgpmon.getCountry(email,passwd,"103.201.129.0")))
+print("\ngetUpstreamInfo():\n"+str(bgpmon.getUpstreamInfo(email,passwd,ip,"AS134829")))
+print("\ngetUpstreamInfoExtensive():\n"+str(bgpmon.getUpstreamInfoExtensive(email,passwd,ip,"AS134829")))
+print("\ngetPrefixes():\n"+str(bgpmon.getPrefixes(email,passwd)))
+print("\ninsertPrefix(): Not yet tested\n")
+print("\nupdatePrefix(): Not yet tested\n")
+print("\ndeletePrefix(): Not yet tested\n")
+print("\ngetPrefixUpstreamList():\n"+str(bgpmon.getPrefixUpstreamList(email,passwd,int(1846488))))
+print("\naddPrefixUpstreamAS(): Not yet tested\n")
+print("\ndeletePrefixUpstreamAS(): Not yet tested\n")
+print("\ngetPrefixAdditionalOriginList():\n"+str(bgpmon.getPrefixAdditionalOriginList(email,passwd,int(1846488))))
+print("\naddPrefixAdditionalOriginAS(): Not yet tested\n")
+print("\ndeletePrefixAdditionalOriginAS(): Not yet tested\n")
+print("\ngetAlerts():\n"+str(bgpmon.getAlerts(email,passwd,int(1))))
+print("\ngetAlertDetails():\n"+str(bgpmon.getAlertDetails(email,passwd,int(1))))
