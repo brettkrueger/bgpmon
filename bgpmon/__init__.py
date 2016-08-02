9129dd70d6c3219178bc8ccc8a65a7573d5219e3
brett <admin@krux.us>
2016-08-02 00:37:49 -0400

    __init__.py commit

--- bgpmon/__init__.py
+++ bgpmon/__init__.py
@@ -1,7 +1,1 @@
 from bgpmon import *
-
-def main():
-	try: from suds.client import Client
-	except: print("Requires suds (suds-jurko)");exit(0)
-	self.url="https://api.bgpmon.net/soap/server.php?wsdl"
-	self.client=Client(self.url)
