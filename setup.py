204c0ec777051592269c77f9ae8e957a5c89afeb
brett <admin@krux.us>
2016-08-02 00:52:08 -0400

    set.py update - GNU License update

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup,find_packages
 
 setup(
     name = "bgpmon",
-    version = "0.1a2",
+    version = "0.1a6",
 	install_requires = ["suds-jurko"],
-    author = "Brett Krueger",
-    author_email = "brett@jinsku.com",
-    description = ("A simple Python wrapper for the BGPMon SOAP API."),
-    license = "GNU",
-    keywords = "bgpmon python wrapper",
-    url = "https://github.com/brettkrueger/bgpmon/",
+	author = "Brett Krueger",
+	author_email = "brett@jinsku.com",
+	description = ("A simple Python wrapper for the BGPMon SOAP API."),
+	license = "GNU",
+	keywords = "bgpmon python wrapper",
+	url = "https://github.com/brettkrueger/bgpmon/bgpmon/",
 	packages=find_packages(exclude=['docs','tests']),
-    classifiers=[
+	classifiers=[
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities :: Internet :: Indexing/Search",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
 		"Programming Language :: Python :: 2",
 		"Programming Language :: Python :: 2.7"
     ],
